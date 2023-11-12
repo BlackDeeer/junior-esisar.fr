03b5dcc70863c1a7f676e6fadb093b195ff97914
Eliot Bas <eliot.bas@orange.fr>
2023-11-12 22:13:47 +0100

    add svgr

--- next.config.js
+++ next.config.js
@@ -15,7 +15,31 @@ const nextConfig = {
         pathname: '/**',
       }
     ]
-  }
+  },
+  experimental: {
+    webpackBuildWorker: true,
+  },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
